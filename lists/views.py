f6c4dcb32c55b18d6a3ea2bea0d4a589d87e1157
Andrey Smirnov <andreypage@yandex.ru>
2020-03-03 12:32:53 +0300

    Новый URL, представление и шаблон для отображения списков

--- lists/views.py
+++ lists/views.py
@@ -5,7 +5,10 @@ def home_page(request):
     '''домашняя страница'''
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
-    
+        return redirect('/lists/one_list_in_world/')
+    return render(request, 'home.html')
+
+def view_list(request):
+    '''новый список'''
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
