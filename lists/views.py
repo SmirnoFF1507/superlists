ee7400d7b51f07c7056d3da5051ec6f20df71d10
Andrey Smirnov <andreypage@yandex.ru>
2020-03-03 23:07:01 +0300

    Добавлен URL для создания нового списка посредством POST

--- lists/views.py
+++ lists/views.py
@@ -3,12 +3,14 @@ from lists.models import Item
 
 def home_page(request):
     '''домашняя страница'''
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/one_list_in_world/')
     return render(request, 'home.html')
 
 def view_list(request):
     '''новый список'''
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    '''новый список'''
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/one_list_in_world/')
