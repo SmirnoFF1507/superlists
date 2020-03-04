8763f8fbf7f3a7465c77b89409bf3c658fae9b50
Andrey Smirnov <andreypage@yandex.ru>
2020-03-04 11:58:54 +0300

    Использован Bootstrap для улучшения макета

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,17 +1,18 @@
 from selenium.common.exceptions import WebDriverException
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
 MAX_WAIT = 10
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     '''тест нового посетителя'''
 
     def setUp(self):
         '''установка'''
         self.browser = webdriver.Firefox()
+        self.browser.set_window_size(766, 768)
 
     def tearDown(self):
         '''демонтаж'''
@@ -21,13 +22,13 @@ class NewVisitorTest(LiveServerTestCase):
         '''тест макета и стилевого оформления'''
         # Эдит открывает домашнюю страницу
         self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
+        self.browser.set_window_size(766, 768)
 
         # Она замечает, что поле ввода аккуратно центрировано
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
+            383,
             delta=10
         )
 
@@ -39,7 +40,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
+            383,
             delta=10
         )
 
