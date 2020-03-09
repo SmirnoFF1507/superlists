4e867093c1004ca45aba8a3fa1be6836c416be59
Andrey Smirnov <andreypage@yandex.ru>
2020-03-09 23:10:23 +0300

    Использована форма во всех представлениях, возврат к рабочему состоянию

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -12,30 +12,35 @@ class ItemValidationTest(FunctionalTest):
         self.browser.get(self.live_server_url)
         self.get_item_input_box().send_keys(Keys.ENTER)
 
-        # Домашняя страница обновляется, и появляется сообщение об ошибке,
-        # которое говорит, что элементы списка не должны быть пустыми
-        self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
-            "Элемент списка не может быть пустым"
+        # Браузер перехватывает запрос и не загружает страницу со списком
+        self.wait_for(lambda: self.browser.find_element_by_css_selector(
+            '#id_text:invalid'
         ))
 
-        # Эдит пробует снова, теперь с неким текстом для элемента, и теперь
-        # это срабатывает
+        # Эдит начинает набирать текст нового элемента и ошибка исчезает
         self.get_item_input_box().send_keys('Купить молоко')
+        self.wait_for(lambda: self.browser.find_element_by_css_selector(
+            '#id_text:valid'
+        ))
+
+        # И она может отправить его успешно
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Купить молоко')
 
         # Как ни странно, Эдит решает отправить второй пустой элемент списка
         self.get_item_input_box().send_keys(Keys.ENTER)
 
-        # Она получает аналогичное предупреждение на странице списка
-        self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
-            "Элемент списка не может быть пустым"
+        # И снова браузер не подчинится
+        self.wait_for_row_in_list_table('1: Купить молоко')
+        self.wait_for(lambda: self.browser.find_element_by_css_selector(
+            '#id_text:invalid'
         ))
 
-        # И она может его исправить, заполнив поле неким текстом
+        # И она может исправиться, заполнив поле текстом
         self.get_item_input_box().send_keys('Сделать чай')
+        self.wait_for(lambda: self.browser.find_element_by_css_selector(
+            '#id_text:valid'
+        ))
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Купить молоко')
         self.wait_for_row_in_list_table('2: Сделать чай')
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -77,17 +77,41 @@ class ListViewTest(TestCase):
 
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
 
-    def test_validation_errors_end_up_on_lists_page(self):
-        """тест: ошибки валидации оканчиваются на странице списков"""
+    def post_invalid_input(self):
+        """отправляет недопустимый ввод"""
         list_ = List.objects.create()
-        response = self.client.post(
+        return self.client.post(
             f'/lists/{list_.id}/',
             data={'text': ''}
         )
+
+    def test_for_invalid_input_nothing_saved_to_db(self):
+        """тест на недопустимый ввод: ничего не сохраняется в БД"""
+        self.post_invalid_input()
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_for_invalid_input_renders_list_template(self):
+        """тест на недопустимый ввод: отображается шаблон списка"""
+        response = self.post_invalid_input()
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'list.html')
-        expected_error = escape("Элемент списка не может быть пустым")
-        self.assertContains(response, expected_error)
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        """тест на недопустимый ввод: форма передается в шаблон"""
+        response = self.post_invalid_input()
+        self.assertIsInstance(response.context['form'], ItemForm)
+
+    def test_for_invalid_input_shows_error_on_page(self):
+        """тест на недопустимый ввод: на странице показывается ошибка"""
+        response = self.post_invalid_input()
+        self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    def test_displays_item_form(self):
+        """тест отображения формы элемента"""
+        list_ = List.objects.create()
+        response = self.client.get(f'/lists/{list_.id}/')
+        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertContains(response, 'name="text"')
 
 
 class NewListTest(TestCase):
