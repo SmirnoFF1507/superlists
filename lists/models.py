6f13bc203fd860270ca2a1df3f2a42c93f6b643a
Andrey Smirnov <andreypage@yandex.ru>
2020-03-09 12:32:37 +0300

    Применена get_absolute_url к модели списка для удаления дублирования url в представлениях (правило DRY)

--- lists/models.py
+++ lists/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.urls import reverse
+
 
 class List(models.Model):
-    '''список'''
-    pass
-     
+    """список"""
+
+    def get_absolute_url(self):
+        """получить абсолютный url"""
+        return reverse('view_list', args=[self.id])
+
+
 class Item(models.Model):
-    '''элемент'''
+    """элемент"""
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
-
--- lists/views.py
+++ lists/views.py
@@ -18,7 +18,7 @@ def view_list(request, list_id):
             item = Item(text=request.POST['item_text'], list=list_)
             item.full_clean()
             item.save()
-            return redirect(f'/lists/{list_.id}/')
+            return redirect(list_)
         except ValidationError:
             error = "Элемент списка не может быть пустым"
 
@@ -36,4 +36,4 @@ def new_list(request):
         list_.delete()
         error = "Элемент списка не может быть пустым"
         return render(request, 'home.html', {"error": error})
-    return redirect(f'/lists/{list_.id}/')
+    return redirect(list_)
