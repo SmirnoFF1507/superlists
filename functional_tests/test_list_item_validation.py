5f708515cbe873d269a8c22811688753ef7fb454
Andrey Smirnov <andreypage@yandex.ru>
2020-03-10 00:24:23 +0300

    Реализована валидация наличия повторяющегося элемента на уровне модели

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -44,3 +44,21 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Купить молоко')
         self.wait_for_row_in_list_table('2: Сделать чай')
+
+    def test_cannot_add_duplicate_items(self):
+        """тест: нельзя добавлять повторяющиеся элементы"""
+        # Эдит открывает домашнюю страницу и начинает новый список
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Купить сапожки')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить сапожки')
+
+        # Она случайно пытается ввести повторяющийся элемент
+        self.get_item_input_box().send_keys('Купить сапожки')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        # Она видит полезное сообщение об ошибке
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "Этот элемент уже есть в списке"
+        ))
